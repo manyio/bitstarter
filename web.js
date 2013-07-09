596f79c180c38beb6a7a0a7c1746e8800b347878
Ramesh Venkataramaiah <ramesh@many.io>
2013-07-09 19:54:05 +0000

    updated index.html

--- web.js
+++ web.js
@@ -11,4 +11,5 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(os.networkInterfaces());
 });
